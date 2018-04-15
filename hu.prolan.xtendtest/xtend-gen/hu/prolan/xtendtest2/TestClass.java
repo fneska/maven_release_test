32cbbd265e5efc24a6c57337eb0e6005d343bca5
Fekete Agnes <Fekete.Agnes@prolan.hu>
2018-04-15 20:00:35 +0200

    s

---
+++ hu.prolan.xtendtest/xtend-gen/hu/prolan/xtendtest2/TestClass.java
@@ -0,0 +1,10 @@
+package hu.prolan.xtendtest2;
+
+import java.io.PrintStream;
+
+@SuppressWarnings("all")
+public class TestClass {
+  public PrintStream testFunction() {
+    return System.out.printf("hello world\n");
+  }
+}
