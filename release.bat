6d5b25baf1070c5c310e38edb42bde88044b3030
Fekete Agnes <Fekete.Agnes@prolan.hu>
2018-04-19 15:38:28 +0200

    w

--- release.bat
+++ release.bat
@@ -1,1 +1,3 @@
-call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=false updateDependencies=true -DtagBase=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test -DurlConnection=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test
+call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=false -DupdateDependencies=true -DtagBase=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test -DurlConnection=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test usern
+
+call mvn release:perform -DconnectionUrl=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test
