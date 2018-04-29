be29db4c3ecd55ae668a29eaa64278cf08eba833
Fekete Agnes <Fekete.Agnes@prolan.hu>
2018-04-29 21:24:05 +0200

    y

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
-call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=false -DupdateDependencies=true -DtagBase=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test
+call mvn release:prepare -DallowTimestampedSnapshots=true -DautoVersionSubmodules=true -DpushChanges=false -DupdateDependencies=true -DreleaseVersion=1.1.13 -DdevelopmentVersion=1.1.14-SNAPSHOT
 
-call mvn release:perform -DconnectionUrl=scm:git:file://C:/users/fekete.agnes/my_github/maven_release_test1/maven_release_test
+call mvn release:perform 
